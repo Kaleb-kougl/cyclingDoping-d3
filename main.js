f193b42e9a2ff20cb48a2771c0d86950eef8f8b8
Kaleb Kougl <kalebkougl@gmail.com>
2018-11-03 10:44:37 -0500

    adding year scale using date datatype for x_axis

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let datasetJSON;
 HTTP.onreadystatechange = function(){
     if (this.readyState == 4 && this.status == 200) {
         dataset = JSON.parse(HTTP.responseText);
-        console.log(dataset);
+        // console.log(dataset);
         d3Commands();
     } else {
         console.log("something went wrong");
@@ -23,20 +23,26 @@ function d3Commands() {
     // Determine axis scales
     const X_MAX = d3.max(dataset, (d) => d['Year']);
     const X_MIN = d3.min(dataset, (d) => d['Year']);
-    const X_SCALE = d3.scaleLinear()
-                      .domain([X_MIN, X_MAX])
-                      .range([(PADDING + PADDING), (WIDTH - PADDING)]);
-    const X_AXIS = d3.axisBottom(X_SCALE);
 
     const TIME_FORMAT = d3.timeFormat("%M:%S");
     const REGEX_SPLIT_TIME = /:/;
     let dateArray = [];
+    let yearArray = []
     for (let i = 0; i < dataset.length; i++){
         let date = new Date(1996, 0, );
+        let year = new Date(dataset[i]['Year'], 0);
+        yearArray.push(year);
         let tempArray = dataset[i]['Time'].split(REGEX_SPLIT_TIME);
         date.setMinutes(parseInt(tempArray[0]), parseInt(tempArray[1]));
         dateArray.push(date);
     }
+
+    const MIN_YEAR = new Date(X_MIN, 0);
+    const MAX_YEAR = new Date(X_MAX, 0);
+    const YEAR_SCALE = d3.scaleTime()
+                         .domain([MIN_YEAR, MAX_YEAR])
+                         .range([(PADDING + PADDING), (WIDTH - PADDING)]);
+    const X_AXIS = d3.axisBottom(YEAR_SCALE);  
 
     const MIN_DATE = dateArray[0];
     const MAX_DATE = dateArray[dateArray.length - 1];
@@ -66,13 +72,13 @@ function d3Commands() {
        .data(dataset)
        .enter()
        .append('circle')
-       .attr('cx', (d, i) => X_SCALE(d['Year']))
+       .attr('cx', (d, i) => YEAR_SCALE(new Date(d['Year'], 0)))
        .attr('cy', (d, i) => Y_SCALE(dateArray[i]))
        .attr('r', 8)
        .attr('fill', 'red')
        .attr('class', 'dot')
-       .attr('data-xvalue', (d, i) => X_SCALE(d['Year']))
-       .attr('data-yvalue', (d, i) => Y_SCALE(dateArray[i]));
+       .attr('data-xvalue', (d, i) => d['Year'])
+       .attr('data-yvalue', (d, i) => dateArray[i]);
 
     svg.append('g')
        .attr('transform', 'translate(' + (PADDING + PADDING) + ', 0)')
